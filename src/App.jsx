93c3a3cbc34ea44d1df4cd9002148c950f3e06b6
Frandy R. Nuñez <ydnarf13@hotmail.com>
2025-04-06 13:21:15 -0400

    updated board logic

--- src/App.jsx
+++ src/App.jsx
@@ -21,19 +21,53 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
-function App() {
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
+function App () {
   const [board, setBoard] = useState(
     Array(9).fill(null)
   )
   const [turn, setTurn] = useState(TURNS.X)
 
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
+
   const updateBoard = (index) => {
+    if(board[index]) return
+    // Update board
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-
+    // Change turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // Check for winner
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
   
   return (
